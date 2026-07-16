7d57bb17d2ecaf71e591856f253a5615b6b641d7
Gino Zampol <ginozampol@Ginos-Mac-mini.local>
2026-07-16 16:24:00 -0500

    Reduce deep forest land value

--- src/core/game/WorldCover.ts
+++ src/core/game/WorldCover.ts
@@ -54,7 +54,7 @@ export const WORLD_COVER_CLASSES: readonly WorldCoverClass[] = [
   {
     magnitude: 10,
     name: "Deep forest",
-    value: 0.2,
+    value: 0.02,
     color: [63, 104, 75],
     resistance: "highland",
   },
